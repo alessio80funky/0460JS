5ddaabe6a381f9662849e3fb6561c0cbf36dec18
alessio80funky <alessio80funky@gmail.com>
2026-08-14 14:58:25 +0900

    8/14 14:58

--- javascript4/data.js
+++ javascript4/data.js
@@ -28,3 +28,55 @@ console.log(typeof code);//number
 console.log("java" + "script");//文字の結合を行われる。動的なデータに向かない書き方。固定文書にしか使えない。
 console.log("java" + 10);///結合されるとナンバー型がストリング型になってしまうため、ナンバー型という型維持したい場合はテンプレート文字列を使う
 
+//それぞれのデータを変数に入れるかそれかテンプレート文字列を使う
+let a = "java";
+let b = 10;
+
+console.log( typeof a + typeof b);
+
+console.log(10 + 10 + "10" + 10 + 10);//20101010　バグ
+
+
+//配列に関して補足
+
+//配列は順番にデータを管理する箱です。
+//             0         1         2
+let fruit = ["apple", "banana", "orange"];///インデックス番号
+
+console.log(fruit)//[ 'apple', 'banana', 'orange' ]
+console.log(fruit[0])  //インスタンスのプロパティの指定
+console.log(fruit[1])
+
+///配列のネスト（配列の中に配列が入っている）
+
+let usr = [
+    // 0      1
+    ["taro", 20],//0番
+     // 0      1
+    ["hanako", 21],//１番
+    //　 0      　1
+    ["hidetada", 28]//２番
+]
+
+console.log(usr[0])//[ 'taro', 20 ]
+console.log(usr[0][0])//taro
+console.log(usr[1][1])//21
+
+
+//オブジェクトに関しての補足
+
+
+let person = {
+//  キー 　値
+    name:"太郎",
+    age:20
+};
+
+console.log(person);//{ name: '太郎', age: 20 }
+console.log(person.name)//太郎
+console.log(person.age)//20
+
+
+//オブジェクトネスト
+
+
--- javascript4/script.js
+++ javascript4/script.js
@@ -9,5 +9,10 @@ const msg = document.getElementById("msg").value;
 
 const res = document.getElementById("result");
 
-    res.textContent = `インポートしたデータ:${name}、${mail}、${msg}`;
+    res.textContent = `【新規受付】
+氏名：${name}様
+メーる：${mail}
+詳細：${msg}
+
+※このメッセージはHTMLから自動送信されました。`;
 })
